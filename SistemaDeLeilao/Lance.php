0cd786a2bf108e640e2ccf0f4c66ea44e650e17f
Arthur Siqueira <arthur.siq5@gmail.com>
2019-07-03 15:47:49 -0300

    criada classe 'Lance'

---
+++ SistemaDeLeilao/Lance.php
@@ -0,0 +1,38 @@
+<?php
+  class Lance{
+    /**
+      * @var \Usuario $usuario
+      */
+    private $usuario;
+    /**
+      * @var mixed $valor
+      */
+    private $valor;
+
+    /**
+      * @param Usuario $usuario
+      * @param float $valor
+      * @return void
+      */
+    function __construct(Usuario $usuario, float $valor){
+      $this->usuario = $usuario;
+      $this->valor = $valor;
+    }
+
+    /**
+      * @access public
+      * @return Usuario $usuario
+      */
+    public function getUsuario():Usuario{
+      return $this->usuario;
+    }
+
+    /**
+      * @access public
+      * @return float $valor
+      */
+    public function getValor():float{
+      return $this->valor;
+    }
+  }
+ ?>
