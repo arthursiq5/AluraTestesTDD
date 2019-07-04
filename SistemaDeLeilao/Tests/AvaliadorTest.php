3d16554ef6de19bc44e1b8d7d276deb3a962543b
Arthur Siqueira <arthur.siq5@gmail.com>
2019-07-04 16:10:36 -0300

    criado teste para médias do leilão

--- SistemaDeLeilao/Tests/AvaliadorTest.php
+++ SistemaDeLeilao/Tests/AvaliadorTest.php
@@ -45,5 +45,24 @@
         */
       $this->assertEquals($menorEsperado, $leiloeiro->getMenorLance());
     }
+    public function testCalculaMedia(){
+      $mediaEsperada = 333.33;
+
+      $leilao = new Leilao('PlayStation 4');
+
+      $renan  = new Usuario('Renan');
+      $caio   = new Usuario('Caio');
+      $felipe = new Usuario('Felipe');
+
+      $leilao->propoe(new Lance($renan,  400));
+      $leilao->propoe(new Lance($caio,   350));
+      $leilao->propoe(new Lance($felipe, 250));
+
+      $leiloeiro = new Avaliador();
+      $leiloeiro->avalia($leilao);
+
+      $this->assertEquals($mediaEsperada, $leiloeiro->getMedia());
+
+    }
   }
  ?>
