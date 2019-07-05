60b13c35c1c8cfe1d466f7cfdd27dc5b96ce8812
Arthur Siqueira <arthur.siq5@gmail.com>
2019-07-05 14:06:09 -0300

    inicia testes sobre classe 'Leilao'

---
+++ SistemaDeLeilao/Tests/LeilaoTest.php
@@ -0,0 +1,16 @@
+<?php
+  use PHPUnit\Framework\TestCase;
+  require_once "{dirname(__FILE__)}/../Usuario.php";
+  require_once "{dirname(__FILE__)}/../Lance.php";
+  require_once "{dirname(__FILE__)}/../Leilao.php";
+  require_once "{dirname(__FILE__)}/../Avaliador.php";
+
+  class LeilaoTest extends TestCase{
+    /**
+      * testa se método 'propor()' está funcionando de forma correta
+      */
+    public function testDeveProporUmLance(){
+      
+    }
+  }
+ ?>
