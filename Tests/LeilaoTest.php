eff72fd764a7429e8116fe8a8a9d9b23ac1c3473
Arthur Siqueira <arthur.siq5@gmail.com>
2019-07-05 16:43:11 -0300

    criada bateria de testes para nova função

--- Tests/LeilaoTest.php
+++ Tests/LeilaoTest.php
@@ -57,5 +57,73 @@
       $this->assertEquals(10, \count($leilao->getLances()));
       $this->assertEquals(7000, \end($leilao->getLances())->getValor());
     }
+
+    public function testDobraLanceAnteriorUsuario(){
+      $leilao = new Leilao("Macbook");
+
+      $jobs = new Usuario('Jobs');
+      $gates = new Usuario('Gates');
+
+      $leilao->propoe(new Lance($jobs , 2000))
+             ->propoe(new Lance($gates, 3000))
+             ->dobraLance($jobs);
+
+      $this->assertEquals(3,    \count($leilao->getLances()));
+      $this->assertEquals(
+        4000,
+        \end($leilao->getLances())->getValor()
+      );
+    }
+    public function testIgnoraDobrasDeLancesNulos(){
+      $leilao = new Leilao("Macbook");
+
+      $jobs = new Usuario('Jobs');
+      $gates = new Usuario('Gates');
+
+
+      $leilao->dobraLance($jobs )
+             ->dobraLance($gates)
+             ->dobraLance($jobs );
+
+      $this->assertEquals(null,    \count($leilao->getLances()));
+    }
+
+    public function testIgnoraDobrasSequenciais(){
+      $leilao = new Leilao("Macbook");
+
+      $jobs = new Usuario('Jobs');
+      $gates = new Usuario('Gates');
+
+      $leilao->propoe(new Lance($jobs , 2000))
+             ->dobraLance($jobs)
+             ->dobraLance($jobs);
+
+      $this->assertEquals(1,    \count($leilao->getLances()));
+      $this->assertEquals(
+        2000,
+        \end($leilao->getLances())->getValor()
+      );
+    }
+
+    public function testIgnoraDobraDeUsuarioComMaisDe5Lances(){
+      $leilao = new Leilao("Macbook");
+
+      $jobs = new Usuario('Jobs');
+      $gates = new Usuario('Gates');
+
+      $leilao->propoe(new Lance($jobs , 2000))
+             ->propoe(new Lance($gates, 3000))
+             ->propoe(new Lance($jobs , 3500))
+             ->propoe(new Lance($gates, 4000))
+             ->propoe(new Lance($jobs , 4500))
+             ->propoe(new Lance($gates, 5000))
+             ->propoe(new Lance($jobs , 5500))
+             ->propoe(new Lance($gates, 6000))
+             ->propoe(new Lance($jobs , 6500))
+             ->propoe(new Lance($gates, 7000))
+             ->dobraLance($jobs);
+      $this->assertEquals(10, \count($leilao->getLances()));
+      $this->assertEquals(7000, \end($leilao->getLances())->getValor());
+    }
   }
  ?>
