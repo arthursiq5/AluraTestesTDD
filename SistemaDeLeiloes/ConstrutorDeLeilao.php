a7cc6ea05fda1afaf9ec8137fe672750896e4a56
Arthur Siqueira <arthur.siq5@gmail.com>
2019-07-08 14:08:31 -0300

    Cria uma factory para Leilões

---
+++ SistemaDeLeiloes/ConstrutorDeLeilao.php
@@ -0,0 +1,41 @@
+<?php
+  require_once './Usuario.php';
+  require_once './Lance.php';
+  require_once './Leilao.php';
+
+  class ConstrutorDeLeilao{
+
+    private $leilao;
+
+    /**
+      * @access public
+      * @param string $descricao
+      * @return ConstrutorDeLeilao $this
+      */
+    public function para($descricao):ConstrutorDeLeilao{
+      $this->leilao = new Leilao($descricao);
+
+      return $this;
+    }
+
+    /**
+      * adiciona item ao leilão
+      * @access public
+      * @param Usuario $usuario
+      * @param float $valor
+      * @return ConstrutorDeLeilao $this
+      */
+    public function lance(Usuario $usuario, float $valor):ConstrutorDeLeilao{
+      $this->leilao->propoe(new Lance($usuario, $valor));
+      return $this;
+    }
+
+    /**
+      * @access public
+      * @return Leilao $leilao
+      */
+    public function constroi():Leilao{
+      return $this->leilao;
+    }
+  }
+ ?>
